2c9fa5fe89c2031d9efd76cb58ed964e0726ef11
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-07-30 17:13:51 +0100

    Updated book_shop.cpp

--- cpp/book_shop.cpp
+++ cpp/book_shop.cpp
@@ -1,28 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
+#include <iostream>
+#include <vector>
+
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
  
 	int n, x;
-	cin >> n >> x;
-	vector<int> h(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> h[i];
-	}
-	vector<int> s(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> s[i];
-	}
- 
-	vector<int> dp(x + 1);
-	for (int i = 0; i < n; ++i) {
-		for (int j = x; j >= h[i]; --j) {
-			dp[j] = max(dp[j], dp[j - h[i]] + s[i]);
-		}
-	}
-	cout << dp[x] << '\n';
+	std::cin >> n >> x;
+
+	std::vector<int> h(n);
+	for (int i = 0; i < n; ++i) std::cin >> h[i];
+
+	std::vector<int> s(n);
+	for (int i = 0; i < n; ++i) std::cin >> s[i];
+
+	std::vector<int> dp(x + 1);
+	for (int i = 0; i < n; ++i)
+		for (int j = x; j >= h[i]; --j)
+			dp[j] = std::max(dp[j], dp[j - h[i]] + s[i]);
+
+	std::cout << dp[x] << '\n';
  
 	return 0;
 }
