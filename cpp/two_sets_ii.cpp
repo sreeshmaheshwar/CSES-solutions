9994a9a9c9ced3f4cc59482e6b20815b37c66cec
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-08-01 14:45:25 +0100

    Updated two_sets_ii.cpp

--- cpp/two_sets_ii.cpp
+++ cpp/two_sets_ii.cpp
@@ -1,24 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 const int MOD = 1e9 + 7;
-const int modular_inverse_2 = 500000004;
- 
+const int MOD_INV_2 = 500000004;
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n; cin >> n;
+
+	int n; 
+	cin >> n;
 	if (n * (n + 1) % 4) {
 		cout << 0 << endl;
-	}
-	else {
-		int target = n * (n + 1) / 4;
-		vector<int> dp(target + 1, 0);
-		dp[0] = 1;
+	} else {
+		int target_sum = n * (n + 1) / 4;
+		vector<long long> ways_to_sum(target_sum + 1, 0);
+		ways_to_sum[0] = 1;
 		for (int i = 1; i <= n; ++i) {
-			for (int j = target - i; j >= 0; --j)
-				dp[i + j] = ((long long) dp[i + j] + dp[j]) % MOD;
+			for (int j = target_sum; j >= i; --j) {
+				ways_to_sum[j] = (ways_to_sum[j] + ways_to_sum[j - i]) % MOD;
+			}
 		}
-		cout << (long long) dp[target] * modular_inverse_2 % MOD << '\n';
+		cout << ways_to_sum[target_sum] * MOD_INV_2 % MOD << endl;
 	}
+
 	return 0;
 }
