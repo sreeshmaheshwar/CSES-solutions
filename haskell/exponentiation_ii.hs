f8846e6e903f8aeabf5329c4f7f628ecafd8ea27
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-08-11 15:51:40 +0100

    Added Exponentiation II haskell solution

--- haskell/exponentiation_ii.hs
+++ haskell/exponentiation_ii.hs
@@ -2,37 +2,41 @@ import Data.Maybe ( fromJust )
 import Data.Char ( isSpace )
 import Control.Monad ( ap, replicateM )
 import qualified Data.ByteString.Char8 as BS
- 
 modulo :: Int
 modulo = 1000000007
  
-modPow :: Int -> Int -> Int
-modPow _ 0
+modPow :: Int -> Int -> Int -> Int
+modPow _ 0 _
   = 1
-modPow a b
-  | odd b     = (evenAnswer * a) `mod` modulo
+modPow a b m
+  | odd b     = (evenAnswer * a) `mod` m
   | otherwise = evenAnswer
   where
-    evenAnswer = (halfPow * halfPow) `mod` modulo
-    halfPow    = modPow a (b `div` 2)
+    evenAnswer = (halfPow * halfPow) `mod` m
+    halfPow    = modPow a (b `div` 2) m
+ 
+solve :: (Int, Int, Int) -> Int
+solve (a, b, c) 
+  = modPow a (modPow b c (modulo - 1)) modulo
  
 main :: IO ()
 main = do
   inputs <- evalState parseInput <$> BS.getContents
-  mapM_ (print . uncurry modPow) inputs
+  mapM_ (print . solve) inputs 
  
 -- FAST INPUT PARSING:
  
-parseInput :: State BS.ByteString [(Int, Int)]
+parseInput :: State BS.ByteString [(Int, Int, Int)]
 parseInput = do
   n <- readInt
-  replicateM n parsePair
- 
-parsePair :: State BS.ByteString (Int, Int)
-parsePair = do
+  replicateM n parseTriple
+  
+parseTriple :: State BS.ByteString (Int, Int, Int)
+parseTriple = do
   a <- readInt
   b <- readInt
-  return (a, b)
+  c <- readInt
+  return (a, b, c)
  
 readInt :: State BS.ByteString Int
 readInt = state $ fromJust . BS.readInt . BS.dropWhile isSpace
