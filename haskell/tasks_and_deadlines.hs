55a47c55835a4f69102d25dd158310955c5f1add
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-08-13 02:14:23 +0100

    Added Tasks and Deadlines haskell solution

---
+++ haskell/tasks_and_deadlines.hs
@@ -0,0 +1,57 @@
+import Data.Maybe ( fromJust )
+import Data.Char ( isSpace )
+import Data.List (sort)
+import Control.Monad ( ap, replicateM )
+import qualified Data.ByteString.Char8 as BS
+
+maxReward :: [(Int, Int)] -> Int 
+maxReward
+  = maxRewardTR 0 0 . sort
+  where
+    maxRewardTR :: Int -> Int -> [(Int, Int)] -> Int
+    maxRewardTR _ curAns []
+      = curAns
+    maxRewardTR curTime curAns ((duration, deadline) : tasks)
+      = maxRewardTR newTime (curAns + deadline - newTime) tasks
+      where 
+        newTime = curTime + duration
+ 
+main :: IO ()
+main = do
+  tasks <- evalState parseInput <$> BS.getContents
+  print . maxReward $ tasks
+ 
+-- FAST INPUT PARSING:
+ 
+parseInput :: State BS.ByteString [(Int, Int)]
+parseInput = do
+  n <- readInt
+  replicateM n parsePair
+ 
+parsePair :: State BS.ByteString (Int, Int)
+parsePair = do
+  a <- readInt
+  b <- readInt
+  return (a, b)
+ 
+readInt :: State BS.ByteString Int
+readInt = state $ fromJust . BS.readInt . BS.dropWhile isSpace
+ 
+newtype State s a = State { runState :: s -> (a, s) }
+ 
+instance Functor (State s) where
+  fmap f m = State $ \s -> let (a, s') = runState m s in (f a, s')
+ 
+instance Applicative (State s) where
+  pure  = return
+  (<*>) = ap
+ 
+instance Monad (State s) where
+  return a = State $ \s -> (a, s)
+  m >>= k  = State $ \s -> let (a, s') = runState m s in runState (k a) s'
+ 
+evalState :: State s a -> s -> a
+evalState m s = fst (runState m s)
+ 
+state :: (s -> (a, s)) -> State s a
+state = State
