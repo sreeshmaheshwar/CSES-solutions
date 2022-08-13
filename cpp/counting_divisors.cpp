76f5ca08e18a07fc631efb27f37eda2b4421acb5
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-08-13 15:32:40 +0100

    Update counting_divisors.cpp

--- cpp/counting_divisors.cpp
+++ cpp/counting_divisors.cpp
@@ -3,32 +3,38 @@
 const int MX_NUM = 1e6;
 int lowest_prime_factor[MX_NUM + 1];
 
+// modified sieve of eratosthenes 
+void precompute_lpfs() {
+	for (int i = 2; i <= MX_NUM; ++i) {
+		if (!lowest_prime_factor[i]) {
+			for (int j = i; j <= MX_NUM; j += i) {
+				if (!lowest_prime_factor[j]) {
+					lowest_prime_factor[j] = i;
+				}
+			}
+		}
+	}
+}
+
 int main() {
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(0);
  	
- 	// modified sieve of eratosthenes that computes lowest prime factors
-	for (int i = 2; i * i <= MX_NUM; ++i) {
-		if (lowest_prime_factor[i]) continue;
-		lowest_prime_factor[i] = i;
-		for (int j = i * i; j <= MX_NUM; j += i) {
-			if (!lowest_prime_factor[j]) lowest_prime_factor[j] = i;
-		}
-	}
- 
+	precompute_lpfs();
 	int n; 
 	std::cin >> n;
 	while (n--) {
 		int x; 
 		std::cin >> x;
-		std::map<int, int> prime_power;
+		std::map<int, int> prime_factor;
 		while (x > 1) {
-			if (!lowest_prime_factor[x]) lowest_prime_factor[x] = x;
-			prime_power[lowest_prime_factor[x]]++;
+			prime_factor[lowest_prime_factor[x]]++;
 			x /= lowest_prime_factor[x];
 		}
 		int ans = 1;
-		for (auto entry : prime_power) ans *= (entry.second + 1);
+		for (auto &[prime, exponent] : prime_factor) {
+			ans *= exponent + 1;
+		}
 		std::cout << ans << '\n';
 	}
  
