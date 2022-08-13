3f07f56460cfc3eab521ac600a4d5ef391cc5768
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-08-13 01:59:24 +0100

    Fixed formatting/style issues

--- cpp/subarray_sums_ii.cpp
+++ cpp/subarray_sums_ii.cpp
@@ -2,27 +2,23 @@
 #include <map>
 #include <vector>
 
-long long count_subarrays_equal_to_x(std::vector<int>& a, int x) {
-	std::map<long long, int> pref_sum_cnt;
-	pref_sum_cnt[0] = 1;
-	long long pref_sum = 0, answer = 0;
-	for (int &element : a) {
-		pref_sum += element;
-		answer += pref_sum_cnt[pref_sum - x];
-		pref_sum_cnt[pref_sum]++;
-	}
-	return answer;
-}
- 
 int main() {
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(0);
  
 	int n, x;
 	std::cin >> n >> x;
-	std::vector<int> a(n);
-	for (int &element : a) std::cin >> element;
-	std::cout << count_subarrays_equal_to_x(a, x) << '\n';
+	std::map<long long, int> pref_sum_cnt;
+	long long cur_pref_sum = 0, answer = 0;
+	pref_sum_cnt[cur_pref_sum]++;
+	for (int i = 0; i < n; ++i) {
+		int element;
+		std::cin >> element;
+		cur_pref_sum += element;
+		answer += pref_sum_cnt[cur_pref_sum - x];
+		pref_sum_cnt[cur_pref_sum]++;
+	}
+	std::cout << answer << '\n';
  
 	return 0;
 }
--- cpp/sum_of_four_values.cpp
+++ cpp/sum_of_four_values.cpp
@@ -10,18 +10,21 @@ int main() {
 	int n, x;
 	std::cin >> n >> x;
 	std::vector<int> a(n);
-	for (int& element : a) std::cin >> element;
-	std::map<int, std::pair<int, int>> sum_position;
+	for (int i = 0; i < n; ++i) {
+		std::cin >> a[i];
+	}
+	std::map<int, std::pair<int, int>> sum_pos;
 	for (int i = 0; i < n; ++i) {
 		for (int j = i + 1; j < n; ++j) {
 			int target_sum = x - a[i] - a[j];
-			if (sum_position.count(target_sum)) {
-				std::cout << i+1 << " " << j+1 << " " << sum_position[target_sum].first+1 << " " << sum_position[target_sum].second+1;
+			if (sum_pos.count(target_sum)) {
+				auto pos = sum_pos[target_sum];
+				std::cout << i + 1 << " " << j + 1 << " " << pos.first + 1 << " " << pos.second + 1;
 				return 0;
 			}
 		}
 		for (int j = 0; j < i; ++j) {
-			sum_position[a[i] + a[j]] = {i, j};
+			sum_pos[a[i] + a[j]] = {i, j};
 		}
 	}
 	std::cout << "IMPOSSIBLE";
