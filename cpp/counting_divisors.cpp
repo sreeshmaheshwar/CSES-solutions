e5f3ee9379c906411a6217ba2dd5aa9bc665c3e8
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-08-15 02:59:41 +0100

    Implemented O(N log N) sieve, simplifying solution

--- cpp/counting_divisors.cpp
+++ cpp/counting_divisors.cpp
@@ -1,42 +1,23 @@
 #include <iostream>
 #include <map>
-const int MX_NUM = 1e6;
-int lowest_prime_factor[MX_NUM + 1];
-
-// modified sieve of eratosthenes 
-void precompute_lpfs() {
-	for (int i = 2; i <= MX_NUM; ++i) {
-		if (!lowest_prime_factor[i]) {
-			for (int j = i; j <= MX_NUM; j += i) {
-				if (!lowest_prime_factor[j]) {
-					lowest_prime_factor[j] = i;
-				}
-			}
-		}
-	}
-}
+const int MAX_X = 1e6;
+int divisor_count[MAX_X + 1];
 
 int main() {
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(0);
  	
-	precompute_lpfs();
+	for (int i = 1; i <= MAX_X; i++) 
+		for (int j = i; j <= MAX_X; j += i) 
+			divisor_count[j]++;
+
 	int n; 
 	std::cin >> n;
 	while (n--) {
 		int x; 
 		std::cin >> x;
-		std::map<int, int> prime_factor;
-		while (x > 1) {
-			prime_factor[lowest_prime_factor[x]]++;
-			x /= lowest_prime_factor[x];
-		}
-		int ans = 1;
-		for (auto &[prime, exponent] : prime_factor) {
-			ans *= exponent + 1;
-		}
-		std::cout << ans << '\n';
+		std::cout << divisor_count[x] << '\n';
 	}
- 
+
 	return 0;
 }
