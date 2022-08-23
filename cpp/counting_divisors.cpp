f75bc5649e0328b527c011af979ff3c5c8fa561f
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-08-23 14:59:45 +0100

    Used std::vector to avoid initialisation issue
    Previous solution assumed an array of ints is initialised to zero

--- cpp/counting_divisors.cpp
+++ cpp/counting_divisors.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <vector>
 const int MAX_X = 1e6;
-int divisor_count[MAX_X + 1];
 
 int main() {
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(0);
- 	
+	
+	std::vector<int> divisor_count(MAX_X + 1, 0);
 	for (int i = 1; i <= MAX_X; i++) 
 		for (int j = i; j <= MAX_X; j += i) 
 			divisor_count[j]++;
