28c0bc9b5778dc1f67cf883d65bc3bb0ec738723
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-09-25 16:29:19 +0100

    Simplified Distinct Values Queries cpp solution

--- cpp/distinct_values_queries.cpp
+++ cpp/distinct_values_queries.cpp
@@ -2,70 +2,58 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <utility>
 
 template<typename T> struct fenwick_tree {
-    std::vector<T> data;
+  private:
     int n;
- 
-    fenwick_tree(int _n, T initial) : n(_n) {
-        data.assign(n, initial);
-    }
- 
+    std::vector<T> tree;
+
+  public:
+    fenwick_tree(int _n) : n(_n), tree(_n, T(0)) {}
+
     T prefix_sum(int r) {
-        T res = 0;
-        for (; r >= 0; r = (r & (r + 1)) - 1)
-            res += data[r];
-        return res;
+        T sum = T(0);
+        while (r >= 0) {
+            sum += tree[r];
+            r = (r & (r + 1)) - 1;
+        }
+        return sum;
     }
- 
+
     T query(int l, int r) { 
         return prefix_sum(r) - prefix_sum(l - 1); 
     }
- 
-    void update(int index, long long delta) { 
-        for (; index < n; index = index | (index + 1))
-            data[index] += delta;
+
+    void update(int i, long long delta) { 
+        while (i < n) {
+            tree[i] += delta;
+            i = i | (i + 1);
+        }
     }
 };
 
-struct query {
-	int l, r, index;
 
-	bool operator<(const query &q) const {
-		return r < q.r;
-	}
-};
- 
 int main(){
-	int n, q; 
-	std::cin >> n >> q; 
-	std::vector<int> a(n);
-	for (int i = 0; i < n; ++i)
-		std::cin >> a[i];
-	std::vector<query> queries;
-	for (int i = 0; i < q; ++i) {
-		int l, r; 
-		std::cin >> l >> r; 
-		l--, r--;
-		queries.push_back(query{l, r, i});
-	}
-	std::sort(queries.begin(), queries.end());
-	fenwick_tree<int> ft(n, 0);
-	std::map<int, int> last_occurance;
-	std::vector<int> answer(q);
-	for (int i = 0, j = 0; i < n && j < q; ++i) {
-		if (last_occurance.count(a[i])) {
-			ft.update(last_occurance[a[i]], -1);
-		}
-		last_occurance[a[i]] = i; 
-		ft.update(i, 1);
-		// answer all queries with this endpoint
-		while (j < q && queries[j].r == i) {
-			answer[queries[j].index] = ft.query(queries[j].l, queries[j].r);
-			j++;
-		}
-	}
-	for (int ans : answer)
-		std::cout << ans << '\n';
-	return 0;
+    int n, q; 
+    std::cin >> n >> q; 
+    std::vector<int> a(n);
+    for (int i = 0; i < n; ++i) std::cin >> a[i];
+
+    std::vector<std::vector<std::pair<int, int>>> with_endpoint(n);
+    for (int i = 0, l, r; i < q; ++i) {
+        std::cin >> l >> r;
+        with_endpoint[r - 1].emplace_back(l - 1, i);
+    }
+
+    fenwick_tree<int> FT(n);
+    std::map<int, int> last_occ;
+    std::vector<int> answer(q);
+    for (int r = 0; r < n; ++r) {
+        if (last_occ.count(a[r])) FT.update(last_occ[a[r]], -1);
+        FT.update(last_occ[a[r]] = r, +1);
+        for (auto [l, i] : with_endpoint[r]) answer[i] = FT.query(l, r);
+    }
+    for (int ans : answer) std::cout << ans << '\n';
+    return 0;
 }
