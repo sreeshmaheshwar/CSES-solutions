bf493dcddecc4f65306e823cb14e2aaaac89978e
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-10-22 23:19:02 +0100

    Minor improvements to Minimizing Coins solution

--- java/dynamic_programming/minimizing_coins/Solution.java
+++ java/dynamic_programming/minimizing_coins/Solution.java
@@ -1,21 +1,24 @@
 import java.util.Arrays;
 import java.util.Scanner;
- 
+
 public class Solution {
- 
-  private final static int INF = (int) 1e6 + 1;
- 
+
+  private final static int INF = (int) 1e7;
+
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt(), x = scanner.nextInt();
+
     int[] minCoinsToSum = new int[x + 1];
     Arrays.fill(minCoinsToSum, 1, x + 1, INF);
+
     for (int i = 0; i < n; ++i) {
       int coin = scanner.nextInt();
       for (int j = coin; j <= x; ++j) {
         minCoinsToSum[j] = Math.min(minCoinsToSum[j], minCoinsToSum[j - coin] + 1);
       }
     }
+
     System.out.println(minCoinsToSum[x] == INF ? -1 : minCoinsToSum[x]);
   }
 }
